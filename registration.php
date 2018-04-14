bb2b5cf618cc9c02ad6c200fc311c875cd00e10a
Sanjay Patel <sanjay@wagento.com>
2018-04-14 10:13:47 +0530

    basic module

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © 2018 Wagento. All rights reserved.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Wagento_Meetup',
+    __DIR__
+);
